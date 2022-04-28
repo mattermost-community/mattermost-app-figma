def838b344a1e4d74558f4b17bc81239bc71d8c3
prokhorind <prokhorind@gmail.com>
2022-04-28 17:59:36 +0300

    add java compile logic inside docker (#21)

--- Dockerfile
+++ Dockerfile
@@ -7,10 +7,13 @@ EXPOSE 8080
 # Refer to Maven build -> finalName
 ARG JAR_FILE
 
+COPY . /opt/app
+
 # cd /opt/app
 WORKDIR /opt/app
 
-COPY ${JAR_FILE} app.jar
+RUN ./mvnw clean install
 
+COPY ${JAR_FILE} app.jar
 # java -jar /opt/app/app.jar
 ENTRYPOINT ["java","-jar","app.jar"]
--- src/test/java/com/mattermost/integration/figma/FigmaApplicationTests.java
+++ src/test/java/com/mattermost/integration/figma/FigmaApplicationTests.java
@@ -2,8 +2,10 @@ package com.mattermost.integration.figma;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {"encryption.key = mySecretKey"})
 class FigmaApplicationTests {
 
 	@Test
