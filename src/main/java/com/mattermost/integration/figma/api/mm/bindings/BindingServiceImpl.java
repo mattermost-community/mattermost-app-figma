8946497f7780238a64bde89a951af122e4cee433
OstapMelnychuk <40043747+OstapMelnychuk@users.noreply.github.com>
2022-09-06 15:08:05 +0300

    Binding fix (#78)

--- src/main/java/com/mattermost/integration/figma/api/mm/bindings/BindingServiceImpl.java
+++ src/main/java/com/mattermost/integration/figma/api/mm/bindings/BindingServiceImpl.java
@@ -5,6 +5,7 @@ import com.mattermost.integration.figma.input.mm.binding.Binding;
 import com.mattermost.integration.figma.input.mm.binding.BindingsDTO;
 import com.mattermost.integration.figma.input.mm.user.MMUser;
 import com.mattermost.integration.figma.input.oauth.InputPayload;
+import com.mattermost.integration.figma.input.oauth.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -34,16 +35,23 @@ public class BindingServiceImpl implements BindingService {
         MMUser currentUser = mmUserService.getUserById(payload.getContext().getActingUser().getId(), mmSiteUrl, botAccessToken);
         String userRoles = currentUser.getRoles();
 
+        User user = payload.getContext().getOauth2().getUser();
+
         if (userRoles.contains(ADMIN_ROLE)) {
             addCommandToBindings(defaultBindings, bindingsProvider.createConfigureBinding());
         }
 
-        if (Objects.nonNull(payload.getContext().getOauth2().getUser().getUserId()) &&
+        if (Objects.isNull(user)) {
+            addCommandToBindings(defaultBindings, bindingsProvider.createConnectBinding());
+            return defaultBindings;
+        }
+
+        if (Objects.nonNull(user.getUserId()) &&
                 (userRoles.contains(TEAM_ADMIN) || userRoles.contains(CHANNEL_ADMIN) || userRoles.contains(ADMIN_ROLE))) {
             addCommandToBindings(defaultBindings, bindingsProvider.createSubscribeBinding());
         }
 
-        if (Objects.isNull(payload.getContext().getOauth2().getUser().getUserId())) {
+        if (Objects.isNull(user.getUserId())) {
             addCommandToBindings(defaultBindings, bindingsProvider.createConnectBinding());
         }
 
